148d7e5393650d71f5eb3324f63998792fd8dd52
867378 <ongvincentdavid@gmail.com>
2025-06-09 16:52:24 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','http://localhost:5174','http://localhost:8080'],
+    'allowed_origins' => ['https://gordon-college-career-hub-frontend.vercel.app'],
 
     'allowed_origins_patterns' => ['*'],
 
